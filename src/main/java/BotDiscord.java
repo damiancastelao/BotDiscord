35fe324feecaaa241f88889b4d5f99f68c45b9e9
Damian Nogueiras <damian@danielcastelao.org>
2022-03-30 14:49:35 +0200

    Manda imagen del disco

--- src/main/java/BotDiscord.java
+++ src/main/java/BotDiscord.java
@@ -1,8 +1,20 @@
+import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.util.Color;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.time.Instant;
+
+import static discord4j.core.object.audit.OptionKey.CHANNEL_ID;
 
 public class BotDiscord {
     public static void main(String[] args) {
@@ -10,11 +22,34 @@ public class BotDiscord {
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .color(Color.GREEN)
+                .title("Yoda")
+                .image("attachment://bbyoda.jpeg")
+                .build();
+
+
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
             final Message message = event.getMessage();
             if ("!ping".equals(message.getContent())) {
                 final MessageChannel channel = message.getChannel().block();
                 channel.createMessage("Pong!").block();
+            }
+            if ("!embed".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+
+                InputStream fileAsInputStream = null;
+                try {
+                    fileAsInputStream = new FileInputStream("bbyoda.jpeg");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                ;
+                channel.createMessage(MessageCreateSpec.builder()
+                        .content("content? content")
+                        .addFile("bbyoda.jpeg", fileAsInputStream)
+                        .addEmbed(embed)
+                        .build()).subscribe();
             }
         });
 
